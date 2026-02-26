5aad547a8d4e4002df0fa8fe4a2bb98c33d0f46d
juliopas <julio.santos.caixote@gmail.com>
2026-02-26 09:00:18 +0000

    added magnetization to recalc_forces on integrate

--- src/core/integrate.cpp
+++ src/core/integrate.cpp
@@ -579,6 +579,10 @@ int System::System::integrate(int n_steps, int reuse_forces) {
     }
 #endif
 
+#ifdef ESPRESSO_MAGNETIZE
+    integrate_magnetodynamics_testing();
+#endif
+
     // Communication step: distribute ghost positions
     cell_structure->update_ghosts_and_resort_particle(get_global_ghost_flags());
 
